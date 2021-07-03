6ff18e22b407a315d55e78c3ec5c827924ce4a97
SA-Bijoy <bijoykhanpp@gmail.com>
2021-07-03 11:22:05 +0600

    large number finding

--- hello.c
+++ hello.c
@@ -8,32 +8,24 @@
 
 int main(){
 
-    int n;
-    printf("Enter row number: ");
-    fflush(stdout);
-    scanf("%d", &n);
+    int multiplication_table[10][10];
 
-    int multiliplication_table[n][10];
+    int col,row;
 
-    int row,col;
-    for(row = 0; row < n; row++){
-        for(col = 0; col < 10; col++){
-            multiliplication_table[row][col] = (row+1)*(col+1);
-        }
-    }
-
-    for(row = 0; row < n; row++){
-        for(col = 0; col < 10; col++){
-            printf("%d x %d = %d\n", row+1, col+1, multiliplication_table[row][col]);
+    for(row = 0; row < 10; row++){
+        for(col  = 0; col < 10; col++){
+            multiplication_table[row][col] = (row+1)*(col+1);
+            printf("%d x %d = %d\n", row+1, col+1, multiplication_table[row][col]);
         }
         printf("\n");
     }
+    
+    int odd = 0,even = 0,sum = 0;
 
-    int i,j,even = 0,odd = 0,sum = 0;
-    for(i = 0; i < n; i++){
-        for(j = 0; j < 10; j++){
-            sum += multiliplication_table[i][j];
-            if(multiliplication_table[i][j]%2 == 0){
+    for(row = 0; row < 10; row++){
+        for(col = 0; col < 10; col++){
+            sum += multiplication_table[row][col];
+            if(multiplication_table[row][col]%2 == 0){
                 even++;
             }else{
                 odd++;
@@ -41,10 +33,20 @@ int main(){
         }
     }
 
-    printf("Even number = %d\n", even);
-    printf("Even number = %d\n", odd);
-    printf("Sum value = %d\n", sum);
+    int large = multiplication_table[0][0];
 
+    for(row = 0; row < 10; row++){
+        for(col = 0; col < 10; col++){
+            if(multiplication_table[row][col] > large){
+                large = multiplication_table[row][col];
+            }
+        }
+    }
+
+    printf("Even value = %d\n", even);
+    printf("Odd value = %d\n", odd);
+    printf("Sum value = %d\n", sum);
+    printf("Large value = %d\n", large);
 
     return 0;
 }
